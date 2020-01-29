0cac24abc5f44136afe99d02bcb96e0a1cc5f191
ashnikel <ashnikel@gmail.com>
2020-01-29 14:24:17 +0300

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello world{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
